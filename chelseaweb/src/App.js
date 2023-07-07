a1e0a48b71648c9035ae4bd7cdc03f2774f6c640
CodeChelseaCode <officialchelsearice@gmail.com>
2023-07-06 21:24:25 -0400

    update 7/6/23

--- chelseaweb/src/App.js
+++ chelseaweb/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Resume from './Resume';
+import Contact from './Contact';
+import Project from './Project';
 
-function App() {
-  return (
-    <div className="Logo">
-      <img src="../WebPics/ChelseaLogo.png" alt="logo" width={500} height={500} />
+// Import your other components (Home, Resume, Project, Contact)
+
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/project" component={Project} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
---
+++ chelseaweb/src/components/resume/resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <h2>Resume</h2>
+      <a href="/path/to/your/resume.pdf" download>
+        Download Resume
+      </a>
+    </div>
+  );
+};
+
+export default Resume;
