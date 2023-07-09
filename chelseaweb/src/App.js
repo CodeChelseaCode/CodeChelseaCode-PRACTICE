391bd0873a642d66f7792ba56d329dbbb7f46462
CodeChelseaCode <officialchelsearice@gmail.com>
2023-07-09 08:29:05 -0400

    restart
    - Needed a new start to refresh memory of react.js Deleted everything and added logo with a page for Home, Resume , Contact & Project.

--- chelseaweb/src/App.js
+++ chelseaweb/src/App.js
@@ -1,27 +1,17 @@
+import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import Resume from './Resume';
-import Contact from './Contact';
-import Project from './Project';
+import Home from './components/home/home.js';
 
-// Import your other components (Home, Resume, Project, Contact)
-
-const App = () => (
-  <Router>
-    <div>
-      <Navbar />
-
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/project" component={Project} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </div>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Home />
+      </div>
+    </Router>  
+    );
+  };
 
 export default App;
---
+++ chelseaweb/src/components/home/home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="navbar">
+        <div className="left-links">
+          <Link to="/">Home</Link>
+          <Link to="/resume">Resume</Link>
+        </div>
+        <div className="right-links">
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        
+      </div>
+      
+      <div className="logo-container">
+        
+          <Link to="/">
+            <img src={require("./ChelseaLogo.png")} alt="logo" className="logo" />
+          </Link>
+          
+      <div className="line"></div>
+      
+      </div>
+      {/* Rest of the home page content */}
+    </div>
+  );
+};
+
+export default Home;
